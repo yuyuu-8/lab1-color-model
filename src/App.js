2d8893d441289ac438372586d82d459468f2bd94
“yuyu“ <nastyastepuk05@gamil.com>
2024-09-17 15:11:53 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Titles from "./Components/Titles";
+import Form from "./Components/Form";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Titles />
+      </div>
+      );
+  }
+}
+
+export default App;
---
+++ src/Components/Titles.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Titles extends React.Component {
+	render() 
+	{
+		return (
+				<div>
+					<h1>Color model</h1>
+					<p>Convert color from one model to another</p>
+				</div>
+			);
+	}
+};
+
+export default Titles;
