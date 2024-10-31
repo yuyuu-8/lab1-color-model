2dcaabfedc947420f28f9742b494e3be721f7f8f
yuyu <sishichulu@gmail.com>
2024-10-31 19:56:20 +0300

    Updates

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ColorPicker from './ColorPicker';
+function App() {
+  return (
+    <div className="App">
+      <h1>Color Picker App</h1>
+      <ColorPicker />
+    </div>
+  );
+}
+
+export default App;
